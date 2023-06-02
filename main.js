ff2848bb8040d28ed4abbcfa29ae45c4c11bb97d
kunu1412 <94915096+kunu1412@users.noreply.github.com>
2023-06-02 11:38:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,119 @@
+
+canvas=new fabric.Canvas("myCanvas")
+
+ball_x=0
+ball_y=0
+hole_x=200
+hole_y=250
+
+
+block_image_width = 5;
+block_image_height = 5;
+
+function new_img(){
+   fabric.Image.fromURL("ball.png",function(Img) {
+		ball_obj = Img;
+	
+		ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
+		ball_obj.set({
+			top:ball_y,
+			left:ball_x
+		});
+		canvas.add(ball_obj)
+	}
+   )
+}
+
+function load_image()
+{
+	fabric.Image.fromURL("golf-h.png",function(Img) {
+		hole_obj = Img;
+	
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
+		hole_obj.set({
+			top:hole_y,
+			left:hole_x
+		});
+		canvas.add(hole_obj);
+	})
+	new_img()
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	if((ball_x==hole_x)&&(ball_y==hole_y)){
+		canvas.remove(ball_obj)
+		document.getElementById("hd3").innerHTML="you have hit the goal";
+		document.getElementById("myCanvas").style.borderBlockColor="red";
+	}
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		if(ball_y>=0){
+			ball_y=ball_y-block_image_height;
+			canvas(ball_obj);
+			new_img()
+		}
+	}
+
+	function down()
+	{
+		 if(ball_y<=450){
+			ball_y=ball_y+block_image_height;
+			canvas.remove(ball_obj);
+			new_img()
+
+		 }
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			ball_x=ball_x-block_image_height;
+			canvas.remove(ball_obj);
+			new_img()
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			ball_x=ball_x+block_image_height;
+			canvas.remove(ball_obj);
+			new_img()
+			
+		}
+	}
+	
+}
